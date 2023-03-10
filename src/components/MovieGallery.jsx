1c4631aa58d1b0f6c1667e8a61962b911d3f232d
AM738349 <96925591+Amirreza10101998@users.noreply.github.com>
2023-03-10 15:38:33 +0000

    local fetch working

--- src/components/MovieGallery.jsx
+++ src/components/MovieGallery.jsx
@@ -11,8 +11,9 @@ class MovieGallery extends Component {
 
     fetchMovies = async () => {
         try {
-            let response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=48442ee&s=${this.props.query}`);
+            let response = await fetch(process.env.REACT_APP_BE_URL);
             if (response.ok) {
+                console.log(response)
                 let data = await response.json();
                 this.setState({ movie: data.Search, isLoading: false });
                 console.log(data);
@@ -22,11 +23,11 @@ class MovieGallery extends Component {
             console.error(error);
         }
     };
-    
-    componentDidMount(){
+
+    componentDidMount() {
         this.fetchMovies()
     }
-    
+
     render() {
         return this.state.isLoading ? (
             <div className="d-flex align-items-center justify-content-center">
@@ -38,31 +39,31 @@ class MovieGallery extends Component {
             </div>
         ) : (
             <Container fluid>
-                <h3 style={{fontSize: '22px'}} className="text-white mt-3">{this.props.query}</h3>
+                <h3 style={{ fontSize: '22px' }} className="text-white mt-3">{this.props.query}</h3>
                 <Carousel className="d-flex  align-items-center mb-5" interval={this.state.interval}>
                     <CarouselItem>
                         <Row>
-                            {this.state.movie.splice(0,6).map(movie => (
-                            <Card className="mx-1 bg-transparent" key={movie.imdbID} style={{ width: "214.02px", height: "120.5px" }}  >
-                                <Card.Img className="img-fluid" style={{width: '100%', objectFit: 'contain'}}  variant="top" src={movie.Poster} />
-                            </Card>
-                          ))}
+                            {this.state.movie.splice(0, 6).map(movie => (
+                                <Card className="mx-1 bg-transparent" key={movie.imdbID} style={{ width: "214.02px", height: "120.5px" }}  >
+                                    <Card.Img className="img-fluid" style={{ width: '100%', objectFit: 'contain' }} variant="top" src={movie.Poster} />
+                                </Card>
+                            ))}
                         </Row>
                     </CarouselItem>
-    
+
                     <CarouselItem>
                         <Row>
                             {this.state.movie.map(movie => (
-                            <Card className="mx-1 bg-transparent" key={movie.imdbID} style={{ width: "214.02px", height: "120.5px" }}  >
-                                <Card.Img className="img-fluid" style={{width: '100%', objectFit: 'contain'}}  variant="top" src={movie.Poster} />
-                            </Card>
-                          ))};
+                                <Card className="mx-1 bg-transparent" key={movie.imdbID} style={{ width: "214.02px", height: "120.5px" }}  >
+                                    <Card.Img className="img-fluid" style={{ width: '100%', objectFit: 'contain' }} variant="top" src={movie.Poster} />
+                                </Card>
+                            ))};
                         </Row>
                     </CarouselItem>
                 </Carousel>
             </Container>
         );
     }
-}    
+}
 
 export default MovieGallery;
