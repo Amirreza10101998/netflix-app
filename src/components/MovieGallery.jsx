cc1a7fff7a3428a04ad5f4ec41a5aa9a2c638b79
AM738349 <96925591+Amirreza10101998@users.noreply.github.com>
2023-03-12 16:55:19 +0000

    .

--- src/components/MovieGallery.jsx
+++ src/components/MovieGallery.jsx
@@ -44,12 +44,13 @@ class MovieGallery extends Component {
                 <Carousel className="d-flex  align-items-center mb-5" interval={this.state.interval}>
                     <CarouselItem>
                         <Row>
-                            {this.state.movie && this.state.movie.map(movie => (
+                            {this.state.movie && this.state.movie.length > 0 && this.state.movie.map(movie => (
                                 <Card className="mx-1 bg-transparent" key={movie.id} style={{ width: "214.02px", height: "120.5px" }}>
                                     <Card.Img className="img-fluid" style={{ width: '100%', objectFit: 'contain' }} variant="top" src={movie.poster} />
                                 </Card>
                             ))}
                         </Row>
+
                     </CarouselItem>
 
 
