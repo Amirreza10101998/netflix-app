f56ed29053528c471a13173da50e0e7000bddda1
AM738349 <96925591+Amirreza10101998@users.noreply.github.com>
2023-03-12 16:48:49 +0000

    changed splice to slice

--- src/components/MovieGallery.jsx
+++ src/components/MovieGallery.jsx
@@ -44,7 +44,7 @@ class MovieGallery extends Component {
                 <Carousel className="d-flex  align-items-center mb-5" interval={this.state.interval}>
                     <CarouselItem>
                         <Row>
-                            {this.state.movie.splice(0, 6).map(movie => (
+                            {this.state.movie.slice(0, 6).map(movie => (
                                 <Card className="mx-1 bg-transparent" key={movie.id} style={{ width: "214.02px", height: "120.5px" }}  >
                                     <Card.Img className="img-fluid" style={{ width: '100%', objectFit: 'contain' }} variant="top" src={movie.poster} />
                                 </Card>
